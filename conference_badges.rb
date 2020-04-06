96a2ecd18a237678893b4ef7e4fc388222a23af1
isabella g henriques <isabellagh88@gmail.com>
2020-04-06 01:35:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+  array.collect do |speaker|
+    badge_maker(speaker)
+  end
+end
+
+def assign_rooms (array)
+  array.each_with_index do |assigned, index|
+    array[array.index(assigned)] = "Hello, #{assigned}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+
+
+
+
+
+  
